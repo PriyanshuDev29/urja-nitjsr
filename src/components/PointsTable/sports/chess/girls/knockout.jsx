429e54759502e45619da09f3602d1b6ca42d3fc3
Aditya <adityaprakash91111@gmail.com>
2025-11-05 13:21:06 +0530

    Some errors resolved

--- src/components/PointsTable/sports/chess/girls/knockout.jsx
+++ src/components/PointsTable/sports/chess/girls/knockout.jsx
@@ -5,7 +5,7 @@ export const tableTennisGirlsKnockout = {
         {
             name: "Final",
             matches: [
-                { id: "ECE+MME", team1: "PG+ME", team2: "Pool 2", winner: "", score1: "", score2: "" },
+                { id: "ECE+MME",date: '13 December', venue: 'TSG', team1: "PG+ME", team2: "Pool 2", winner: "", score1: "", score2: "" },
             ]
         },
         {
